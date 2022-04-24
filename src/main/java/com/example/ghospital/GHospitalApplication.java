aa0854de27c03cf6f9c11a84fc6c206b87014124
ayman rah <rahmani.ayman17@gmail.com>
2022-04-24 17:07:41 +0100

    first commit

---
+++ src/main/java/com/example/ghospital/GHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.ghospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GHospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GHospitalApplication.class, args);
+    }
+
+}
